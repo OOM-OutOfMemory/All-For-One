688178ffd0640c236f3db7a17c36f656f762d6a2
yusong-offx <hssong1024@gmail.com>
2025-07-28 05:40:16 +0900

    [style] modify validation function comments
    Add config validation stub function

---
+++ src/config/validation.rs
@@ -0,0 +1,9 @@
+use crate::config::Config;
+use anyhow::Result;
+
+// if you need,
+// you can add validation logic here
+pub(super) fn check_config_validation(config: Config) -> Result<Config> {
+    // logic
+    Ok(config)
+}
