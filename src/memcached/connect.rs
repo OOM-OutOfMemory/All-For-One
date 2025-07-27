51b8a710678f7dea90a148b49333a43e4caf0294
yusong-offx <hssong1024@gmail.com>
2025-07-28 05:40:39 +0900

    [feat] implement memcached connect function
    Add memcached_connect function with error context

--- src/memcached/connect.rs
+++ src/memcached/connect.rs
@@ -1,1 +1,8 @@
-pub async fn memcached_connect() {}
+use anyhow::{Context, Result};
+use memcache::Client;
+use url::Url;
+
+pub fn memcached_connect(url: Url, pool_size: u32) -> Result<Client> {
+    Ok(memcache::Client::with_pool_size(url, pool_size)
+        .context("Fail to connect memcached server")?)
+}
