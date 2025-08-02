bd39c4bafcede427e6d31369219361f0abc5e164
yusong-offx <hssong1024@gmail.com>
2025-08-02 10:22:09 +0900

    [feat] make database module
    Add initial database module with connect and repo submodules

---
+++ src/db/connect.rs
@@ -0,0 +1,37 @@
+use std::time::Duration;
+
+use anyhow::{Context, Result};
+use sea_orm::DatabaseConnection;
+use tracing::info;
+
+use crate::config::types::Config;
+
+pub async fn postgres_connect(config: &Config) -> Result<DatabaseConnection> {
+    let connect_info = &config.postgres.connect_info;
+    let database_url = url::Url::parse(
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            connect_info.username,
+            connect_info.password,
+            connect_info.address,
+            connect_info.port,
+            connect_info.db_name
+        )
+        .as_str(),
+    )?;
+
+    let runtime_options = &config.postgres.runtime_options;
+    let mut opt = sea_orm::ConnectOptions::new(database_url.to_string());
+    opt.min_connections(runtime_options.min_pool_size)
+        .connect_timeout(Duration::from_secs(runtime_options.connect_timeout))
+        .acquire_timeout(Duration::from_secs(runtime_options.acquire_timeout))
+        .idle_timeout(Duration::from_secs(runtime_options.idle_timeout))
+        .max_lifetime(Duration::from_secs(runtime_options.max_lifetime))
+        .sqlx_logging(runtime_options.sqlx_logging);
+
+    let db = sea_orm::Database::connect(opt)
+        .await
+        .context("fail to database connection")?;
+
+    Ok(db)
+}
