0022b68397c776db9f3b8a7101a1753b5265e399
yusong-offx <hssong1024@gmail.com>
2025-08-06 08:22:14 +0900

    [feat] add idp column in Users table
    Add idp field to users Model

--- src/entity/users.rs
+++ src/entity/users.rs
@@ -14,6 +14,7 @@ pub struct Model {
     pub is_active: bool,
     pub created_at: DateTime,
     pub updated_at: DateTime,
+    pub idp: String,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
