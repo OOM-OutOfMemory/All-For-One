445ba8d2e6dadb174e327f58fe3b405638987ad0
yusong-offx <hssong1024@gmail.com>
2025-08-07 12:01:23 +0900

    [feat] add jwt_issuer in app_state
    Add JwtIssuer to AppState initialization

--- src/api/state/init.rs
+++ src/api/state/init.rs
@@ -13,10 +13,12 @@ pub async fn make_app_state(config: &Config) -> Result<AppState> {
     let postgres_state = Arc::new(postgres_connect(config).await?);
     let oauth_provider_state = Arc::new(OAuthProviderClient::new(config)?);
     let memcached_state = Arc::new(memcached_connect(config)?);
+    let jwt_issuer = Arc::new(JwtIssuer::new(config).await?);
 
     Ok(AppState {
         oauth_provider_state,
         postgres_state,
         memcached_state,
+        jwt_issuer,
     })
 }
